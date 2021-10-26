af9708bfb2c9432607b9d6b2d9a389cbbfc804f0
Toni Hukka <berendin@gmail.com>
2021-10-26 15:41:32 +0300

    added total km and duration info, testing dense table

--- frontend/src/components/StatPage.js
+++ frontend/src/components/StatPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navigation from './Navigation';
-import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Stack } from '@mui/material';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import { styled } from '@mui/material/styles';
 
 const dummyData = [
   {
@@ -23,6 +25,13 @@ const dummyData = [
   }
 ];
 
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  textAlign: 'center',
+  padding: 40,
+  width: '50%'
+}));
+
 const StatPage = () => {
   return (
     <Box
@@ -32,14 +41,22 @@ const StatPage = () => {
         flexDirection: 'column',
       }}
     >
+      <Stack direction='row' justifyContent='center' alignItems='center' spacing={2} mb={4} >
+        <Item><strong>Total km</strong><br />
+        500km</Item>
+        <Item><strong>Total duration</strong><br />
+        60 hours</Item>
+      </Stack>
+
       <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 450 }} aria-label='simple table'>
+        <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
           <TableHead>
             <TableRow>
               <TableCell>Date</TableCell>
               <TableCell>Kilometers</TableCell>
               <TableCell>Duration (minutes)</TableCell>
               <TableCell>Difficulty</TableCell>
+              <TableCell>Delete</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -51,6 +68,7 @@ const StatPage = () => {
                 <TableCell>{entry.kilometers}</TableCell>
                 <TableCell>{entry.duration}</TableCell>
                 <TableCell>{entry.difficulty}</TableCell>
+                <TableCell><DeleteForeverIcon /></TableCell>
               </TableRow>
             ))}
           </TableBody>
