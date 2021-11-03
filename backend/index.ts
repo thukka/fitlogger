6a384b17eddf4ea42ede0683fb6b18b3804d7a35
Toni Hukka <berendin@gmail.com>
2021-11-03 09:54:13 +0200

    mongo connection established, new routes, installed bcrypt, morgan, jwt

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,27 @@
+require('dotenv').config();
 import express from 'express';
+import entryRouter from './src/routes/entry';
+import usersRouter from './src/routes/users';
+import loginRouter from './src/routes/login';
+import { connect } from 'mongoose';
+
+import morgan from 'morgan';
+
+const mongoUrl = process.env.MONGO_URI;
 const app = express();
+
+const run = async(): Promise<void> => {
+    await connect(mongoUrl as string);
+};
+
+run().catch(err => console.log(err));
+
+app.use(morgan('tiny'));
 app.use(express.json());
 
-app.get('/ping', (_req, res) => {
-    res.send('pong');
-});
+app.use('/api/entry', entryRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 const PORT = 3003;
 
