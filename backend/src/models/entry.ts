db4290d17605dd95a46f429f14f0b0aea95c6bbb
Toni Hukka <berendin@gmail.com>
2021-12-13 16:28:55 +0200

    mongoose model for user entries

---
+++ backend/src/models/entry.ts
@@ -0,0 +1,33 @@
+import { Entry } from '../types';
+import mongoose, { Schema } from 'mongoose';
+
+const entrySchema = new Schema<Entry>({
+    user: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    distance: {
+        type: Number,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    difficulty: Number
+})
+
+entrySchema.set('toJSON', {
+    transform: (_document, returnedObject) => {
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    }
+})
+
+const Entry = mongoose.model<Entry>('Entry', entrySchema);
+
+export default Entry;
