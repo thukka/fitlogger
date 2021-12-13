75537c1c5049859f87c76dd3e49940e8e8109f46
Toni Hukka <berendin@gmail.com>
2021-12-13 16:14:25 +0200

    Added entry and jwtpayload types

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -5,6 +5,21 @@ export interface User {
     toJSON: () => string
 }
 
+export interface Entry {
+    user: string,
+    date: string,
+    duration: number,
+    distance: number,
+    difficulty?: number
+}
+
 export interface NewUser extends Omit<User, 'toJSON' | 'passwordHash'> {
     password: string
 };
+
+export interface JwtPayload {
+    id: string,
+    username: string,
+    iat: number,
+    exp: number,
+}
