44f02de13a478fb05778c7b6a607e50c7f6faa57
Toni Hukka <berendin@gmail.com>
2021-12-13 16:19:44 +0200

    added states for user and error msg, dom router moved, useEffect for setting user if user can be found in local storage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import LoginPage from './components/LoginPage';
 import FrontPage from './components/FrontPage';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import StatPage from './components/StatPage';
+import Notification from './components/Notification';
 
 const App = () => {
 
-  return (
-    <Router>
+  const [error, setError] = useState(null);
+  const [user, setUser] = useState(null);
+  console.log('user: ', user);
 
-      <Switch>
-        <Route path='/frontpage'>
-          <FrontPage />
-        </Route>
-        <Route path='/stats'>
-          <StatPage />
-        </Route>
-        <Route path='/'>
-          <LoginPage />
-        </Route>
-      </Switch>
-    </Router>
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser');
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+    }
+  }, []);
+
+  const SetErrorMessage = (msg) => {
+    setError(msg);
+    setTimeout(() => {
+      setError(null);
+    }, 5000);
+  };
+
+  return user !== null ? (
+    <Switch>
+      <Route path='/frontpage'>
+        <FrontPage user={user} />
+      </Route>
+      <Route path='/stats'>
+        <StatPage />
+      </Route>
+      <Route path='/'>
+        {user !== null ? <Redirect to='/frontpage' /> : <LoginPage setUser={setUser} />}
+      </Route>
+    </Switch>
+  ) : (
+    <>
+      {error ? <Notification error={error} /> : null}
+      <LoginPage setUser={setUser} SetErrorMessage={SetErrorMessage} />
+    </>
   );
 };
 
