47eb3ffce7839837462b7646cb8d1f2e511eb85a
Toni Hukka <berendin@gmail.com>
2021-12-13 16:25:24 +0200

    one line for mui alerts

--- frontend/src/components/Notification.js
+++ frontend/src/components/Notification.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Alert from '@mui/material/Alert';
-import { AlertTitle } from '@mui/material';
+import { Alert, AlertTitle } from '@mui/material';
 
 const Notification = ({ error }) => {
   return (
