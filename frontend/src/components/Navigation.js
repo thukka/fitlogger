d69e10e80dcbaa3ed523c2219c621a04f6f73c21
Toni Hukka <berendin@gmail.com>
2021-12-13 16:22:59 +0200

    added logout function for clearing localstorage and reloading the page

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -7,15 +7,19 @@ import LogoutIcon from '@mui/icons-material/Logout';
 
 const Navigation = () => {
 
+  const LogOutUser = () => {
+    window.localStorage.clear();
+    window.location.reload();
+  };
+
   return (
     <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
       <BottomNavigation
         showLabels
       >
-
         <BottomNavigationAction component={Link} to='/frontpage' label='Frontpage' icon={<HomeIcon sx={{ color: 'lightblue' }} />} />
         <BottomNavigationAction component={Link} to='/stats'label='Stats' icon={<BarChartIcon sx={{ color: 'green' }} />} />
-        <BottomNavigationAction component={Link} to='/' label='Logout' icon={<LogoutIcon sx={{ color: 'darkred' }} />} />
+        <BottomNavigationAction onClick={LogOutUser} label='Logout' icon={<LogoutIcon sx={{ color: 'darkred' }} />} />
       </BottomNavigation>
     </Paper>
   );
