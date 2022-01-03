9aecaf96d5aff3cfb9f35b9de6cea10cb155bfc1
Toni Hukka <berendin@gmail.com>
2022-01-03 09:48:45 +0200

    changed user state to use redux state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,21 @@ import FrontPage from './components/FrontPage';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import StatPage from './components/StatPage';
 import Notification from './components/Notification';
+import { useSelector, useDispatch } from 'react-redux';
+import { setUser } from './reducers/userReducer';
 
 const App = () => {
 
   const [notification, setNotification] = useState(null);
   const [isError, setIsError] = useState(false);
-  const [user, setUser] = useState(null);
+  const user = useSelector(state => state);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedUser');
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON);
-      setUser(user);
+      dispatch(setUser(user));
     }
   }, []);
 
@@ -31,19 +34,19 @@ const App = () => {
     <Switch>
       <Route path='/frontpage'>
         {notification ? <Notification error={isError} notification={notification} /> : null}
-        <FrontPage user={user} setNotificationMessage={setNotificationMessage} />
+        <FrontPage setNotificationMessage={setNotificationMessage} />
       </Route>
       <Route path='/stats'>
-        <StatPage user={user} />
+        <StatPage />
       </Route>
       <Route path='/'>
-        {user !== null ? <Redirect to='/frontpage' /> : <LoginPage setUser={setUser} />}
+        {user !== null ? <Redirect to='/frontpage' /> : <LoginPage />}
       </Route>
     </Switch>
   ) : (
     <>
       {notification ? <Notification error={isError} notification={notification} /> : null}
-      <LoginPage setUser={setUser} setNotificationMessage={setNotificationMessage} setIsError={setIsError} />
+      <LoginPage setNotificationMessage={setNotificationMessage} setIsError={setIsError} />
     </>
   );
 };
