6e4f09ec6f17b704b38f5f5cd039bbcb0c3a134c
Toni Hukka <berendin@gmail.com>
2022-01-06 12:17:55 +0200

    added test for RESET_NOTIFICATION

--- frontend/src/reducers/notificationReducer.test.js
+++ frontend/src/reducers/notificationReducer.test.js
@@ -22,4 +22,22 @@ describe('notificationReducer', () => {
     const stateAfterError = notificationReducer(state, errorAction);
     expect(stateAfterError.isError).toBe(true);
   });
+
+  test('state is reset with action RESET_NOTIFICATION', () => {
+    const action = {
+      type: 'SET_NOTIFICATION',
+      message: 'hello world',
+      error: true
+    };
+    deepFreeze(state);
+
+    const newState = notificationReducer(state, action);
+    expect(newState.message).toBe('hello world');
+    expect(newState.isError).toBe(true);
+    const resetNotification = {
+      type: 'RESET_NOTIFICATION'
+    };
+    const stateAfterReset = notificationReducer(state, resetNotification);
+    expect(stateAfterReset).toStrictEqual(state);
+  });
 });
