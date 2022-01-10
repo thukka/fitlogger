bf87309f70ead5000655b7ffefdeccb20e151eb9
Toni Hukka <berendin@gmail.com>
2022-01-10 16:28:18 +0200

    added new types for user login and new entries

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -2,7 +2,13 @@ export interface User {
     name: string,
     passwordHash: string,
     email: string,
-    toJSON: () => string
+    toJSON: () => string,
+    _id?: string
+}
+
+export interface UserLoginCredentials {
+    email: string,
+    password: string,
 }
 
 export interface Entry {
@@ -13,13 +19,31 @@ export interface Entry {
     difficulty?: number
 }
 
+export interface UnknownEntry {
+    user: unknown,
+    date: unknown,
+    duration: unknown,
+    distance: unknown,
+    difficulty?: unknown
+}
+
 export interface NewUser extends Omit<User, 'toJSON' | 'passwordHash'> {
     password: string
-};
+}
 
 export interface JwtPayload {
     id: string,
     username: string,
     iat: number,
     exp: number,
+}
+
+export interface MongoReturnedObjectEntry {
+    id?: string,
+    __v?: string,
+    _id?: string,
+}
+
+export interface MongoReturnedObjectUser extends MongoReturnedObjectEntry {
+    passwordHash?: string
 }
