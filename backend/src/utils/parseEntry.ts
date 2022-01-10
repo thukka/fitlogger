68aedebfcacd4fc4c6b453c294cdaf1d54b0196e
Toni Hukka <berendin@gmail.com>
2022-01-10 16:30:30 +0200

    parse tool for new entries

---
+++ backend/src/utils/parseEntry.ts
@@ -0,0 +1,40 @@
+import parseString, { isString } from "./parseString";
+import { Entry, UnknownEntry } from '../types';
+
+const isNumber = (n: unknown): n is number => {
+    return typeof n === 'number' || n instanceof Number;
+};
+
+const parseNumber = (n: unknown): number => {
+    if (!isNumber(n)) {
+        throw new Error(`Not a number! ${n}`);
+    }
+    return n;
+};
+
+const isDate = (date: string): boolean => {
+    return Boolean(Date.parse(date));
+};
+
+const parseDate = (date: unknown): string => {
+    if (!date || !isString(date) || !isDate(date)) {
+        throw new Error('Incorrect or missing date' + date);
+    }
+    return date;
+};
+
+const parseEntry = (body : UnknownEntry): Entry => {
+    if (!body.user || !body.date || !body.duration || !body.distance || !body.difficulty) {
+        throw new Error('Field is missing!');
+    }
+    const newEntry: Entry = {
+        user: parseString(body.user),
+        date: parseDate(body.date),
+        duration: parseNumber(Number(body.duration)),
+        distance: parseNumber(Number(body.distance)),
+        difficulty: parseNumber(Number(body.difficulty))
+    };
+    return newEntry;
+};
+
+export default parseEntry;
