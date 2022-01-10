3b2f449f6cacc3d3fa229fc1ef8f5cb4ea42ae2e
Toni Hukka <berendin@gmail.com>
2022-01-10 16:31:32 +0200

    added types for variables

--- backend/src/routes/login.ts
+++ backend/src/routes/login.ts
@@ -3,25 +3,27 @@ import User from '../models/user';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 const router = express.Router();
+import { User as UserType, UserLoginCredentials } from '../types';
+import parseString from '../utils/parseString';
 
 router.post('/', async (req, res) => {
-    const body = req.body;
-    const user: any = await User.findOne({ email: body.email });
+    const body = req.body as UserLoginCredentials;
+    const user: UserType | null = await User.findOne({ email: body.email });
 
     const passwordCorrect = user === null
         ? false
-        : await bcrypt.compare(body.password, user.passwordHash);
+        : await bcrypt.compare(parseString(body.password), user.passwordHash);
 
     if (!(user && passwordCorrect)) {
         return res.status(401).json({
             error: 'Invalid username or password'
-        })
-    };
+        });
+    }
 
     const userForToken = {
         username: user.email,
         id: user._id,
-    }
+    };
 
     const token = jwt.sign(userForToken, process.env.SECRET as string, { expiresIn: 60 * 60 });
 
