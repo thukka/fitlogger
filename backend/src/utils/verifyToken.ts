571535c0d09370712ffc895432bbc8ce2e7043b9
Toni Hukka <berendin@gmail.com>
2022-01-24 11:18:53 +0200

    check that the token contains bearer info

--- backend/src/utils/verifyToken.ts
+++ backend/src/utils/verifyToken.ts
@@ -1,15 +1,27 @@
 import jwt from 'jsonwebtoken';
 import { JwtPayload } from '../types';
 
-const verifyToken = (token: string) => {
-    let decodedToken;
+const tokenExtractor = (token: string) => {
+    if (token.startsWith('bearer ')) {
+        return token.substring(7);
+    } else {
+        throw new Error('Malformed token.');
+    }
+};
 
+const verifyToken = (token: string) => {
+    try {
+        token = tokenExtractor(token);
+    } catch (err) {
+        return undefined;
+    }
+
+    let decodedToken;
     try {
         decodedToken = jwt.verify(token, process.env.SECRET as string) as JwtPayload;
     } catch ({ message }) {
         console.log('Something went wrong when verifying token: ', message);
     }
-
     return decodedToken;
 };
 
